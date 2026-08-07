b2b1a0d150af6bcf54e1f432b13e3dfdeb8eed92
Anirudh Patil <anirudhpatil2712@gmail.com>
2026-08-07 20:12:30 +0530

    Add ticket list with protected API

--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -1,4 +1,45 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Tickets(){
-    return <h1>Tickets Pages</h1>
+    const [tickets, setTickets] =  useState([]);
+
+    useEffect(() => {
+        const fetchtickets = async () =>{
+            try{
+                const response = await api.get("/tickets");
+
+                console.log("TICKETS:" , response.data);
+
+                setTickets(response.data.data);
+            }catch(error){
+                console.log("TICKETS ERROR:" , error);
+            }
+        };
+        fetchtickets();
+   },[]);
+
+   return(
+    <div>
+        <h1>Tickets</h1>
+
+        {tickets.map((ticket) =>(
+            <div key={ticket._id}>
+
+                <h3>{ticket.ticketId}</h3>
+
+                <p>Customer : {ticket.customerName}</p>
+
+                <p>Subject:{ticket.subject}</p>
+
+                <p>Status: {ticket.status}</p>
+
+                <hr/>            
+                            
+                </div>
+        ))}
+    </div>
+   );
 }
+
 export default Tickets;
