52afc88aeb38fb66b9cdac5df61995327a13a917
Anirudh Patil <anirudhpatil2712@gmail.com>
2026-08-10 12:45:58 +0530

    feat: protect authenticated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,26 +6,55 @@ import Tickets from "./pages/Tickets";
 import CreateTicket from "./pages/CreateTicket";
 import EditTicket from "./pages/EditTicket";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
+
+       
         <Route path="/" element={<Login />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path="/tickets" element={<Tickets />} />
+        <Route
+          path="/tickets"
+          element={
+            <ProtectedRoute>
+              <Tickets />
+            </ProtectedRoute>
+          }
+        />
 
         <Route
           path="/tickets/create"
-          element={<CreateTicket />}
+          element={
+            <ProtectedRoute>
+              <CreateTicket />
+            </ProtectedRoute>
+          }
         />
 
         <Route
           path="/tickets/edit/:id"
-          element={<EditTicket />}
+          element={
+            <ProtectedRoute>
+              <EditTicket />
+            </ProtectedRoute>
+          }
         />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
